9052052d81f60e007c457c289d95efcae2bbebfa
Alexand0x <Alexand0x@users.noreply.github.com>
2017-07-15 11:30:03 -0400

    Update user-warner.py

--- user-warner.py
+++ user-warner.py
@@ -10,7 +10,7 @@ def get_users():
 users = get_users()
 
 user_whitelist = []
-user_blacklist = ['jarrad', 'jared', 'carl', 'hope', 'bennetts', 'status', 'vip', 'group', 'official', 'tatus', 'satus', 'sttus', 'staus', 'stats', 'statu', 'sstatus', 'sttatus', 'staatus', 'stattus'] # truncated
+user_blacklist = ['alexander', 'joe', 'matus', 'urgo', 'madvas', 'joeyurgz', 'district0x', 'vip', 'group', 'official', 'district', '0x', 'distrit', 'distric', 'ditrict', 'district0', 'distric0x', 'diistrict0x', 'd0x', 'disrict0x'] # truncated
 id_whitelist = ['U0PDFC7EX', 'U0PF69M34']
 for user in users:
     name = user['name']
@@ -18,6 +18,6 @@ for user in users:
         for b in user_blacklist:
             if b in name:
                 if user['id'] not in id_whitelist:
-                    slack.chat.post_message('@{}'.format(name), 'Hi, in an effort to protect against scammers and fraud, please change your username to not include "status", "vip", "official" or be similar to "jarrad" or "carl". This is an automated message and you will be banned by end of week if your username is not changed. Thank-you for understanding.', as_user=True)
+                    slack.chat.post_message('@{}'.format(name), 'Hi, in an effort to protect against scammers and fraud, please change your username to not include "district0x", "vip", "official" or be similar to "alexander", "madvas" or "joe". This is an automated message and you will be banned by end of day if your username is not changed. Thank you for understanding.', as_user=True)
                     # print(name)
                     break
