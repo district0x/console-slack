36a20b0068ad4acb42da298b184487b8bea9cfa4
Alexand0x <Alexand0x@users.noreply.github.com>
2017-07-15 11:28:11 -0400

    Update Moderator.py

--- plugins/Moderator.py
+++ plugins/Moderator.py
@@ -19,9 +19,9 @@ class Moderator(Plugin):
     flagged_users = {}
     eth_prog = re.compile(r'((0x)?[0-9a-fA-F]{40})')
     btc_prog = re.compile(r'([13][a-km-zA-HJ-NP-Z1-9]{25,34})')
-    url_whitelist = ['contribute.status.im', 'test.status.im', 'slack.status.im', 'blog.status.im', 'status-im.slack.com', 'hackathon.status.im', 'contribute.status.im', 'status.im', 'www.status.im', 'wiki.status.im', 'artifacts.status.im', 'docs.status.im', 'www.bitcoinsuisse.ch', 'bitcoinsuisse.ch', 'i.diawi.com', 'commiteth.com', 'www.commiteth.com']
+    url_whitelist = ['contribute.district0x.io', 'test.district0x.io', 'slack.district0x.io', 'blog.district0x.io', 'district0x.slack.com', 'contribute.district0x.io', 'district0x.io', 'district0x.io/docs/district0x-whitepaper.pdf', 'twitter.com/distrct0x', 'github.com/district0x']
     user_whitelist = [] # truncated left as example
-    user_blacklist = ['jarrad', 'jared', 'carl', 'hope', 'bennetts', 'status', 'vip', 'group', 'official' ] # truncated left as example
+    user_blacklist = ['joe', 'joeyurgz', 'madvas', 'matus', 'alexander', 'district0x', 'vip', 'group', 'official' ] # truncated left as example
     id_whitelist = ['U4FASQ3PS'] # truncated left as example
     chan_whitelist = ['G2B3WG3LG', 'G5DA06HB7'] # truncated left as example
     time_lapse = 3600
